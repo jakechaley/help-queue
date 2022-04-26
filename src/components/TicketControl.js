95268bd7e70528c95329a097dcd8d605250ac3bd
jakechaley <jakechaley@gmail.com>
2022-04-26 15:55:57 -0700

    add action creators and action constants

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from "./TicketDetail";
 import EditTicketForm from "./EditTicketForm";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import * as a from "./../actions";
 
 
 class TicketControl extends React.Component {
@@ -18,20 +19,14 @@ class TicketControl extends React.Component {
 
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: "ADD_TICKET",
-      id: id,
-      names: names,
-      location: location,
-      issue: issue
-    }
+    const action = a.addTicket(ticketToEdit);
     dispatch(action);
     this.setState({
       editing: false,
       selectedTicket: null
     });
   }
+
 
   handleEditClick = () => {
     console.log("handleEditClick reached!");
@@ -40,10 +35,7 @@ class TicketControl extends React.Component {
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: "DELETE_TICKET",
-      id: id,
-    }
+    const action = a.deleteTicket(id) 
     dispatch(action);
     this.setState({selectedTicket: null});
   }
@@ -55,20 +47,12 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: "ADD_TICKET",
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(newTicket);
     dispatch(action);
-    const action2 = {
-      type: "TOGGLE_FORM"
-    }
+    const action2 = a.toggleForm();
     dispatch(action2);
   }
+
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
@@ -77,9 +61,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: "TOGGLE_FORM"
-      }
+      const action = a.toggleForm();
       dispatch(action);
     }
   }
@@ -115,10 +97,6 @@ class TicketControl extends React.Component {
     );
   }
 }
-
-TicketControl.propTypes = {
-  mainTicketList: PropTypes.object
-};
 
 const mapStateToProps = state => {
   return {
