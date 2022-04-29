157ec65ee81d9c8ec3012f6c8fd1648f0607103a
jakechaley <jakechaley@gmail.com>
2022-04-28 17:20:20 -0700

    get help-queue to use moment.js for time stamping tickets

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -2,18 +2,18 @@ import * as c from "./../actions/ActionTypes";
 
 export default (state = {}, action) => {
   const { names, location, issue, id, formattedWaitTime, timeOpen } = action;
-  switch(action.type) {
-    case c.ADD_TICKET:
-      return Object.assign({}, state, {
-        [id]: {
-          names: names,
-          location: location,
-          issue: issue,
-          id: id,
-          timeOpen: timeOpen,
-          formattedWaitTime: formattedWaitTime
-        }
-      });
+  switch (action.type) {
+  case c.ADD_TICKET:
+    return Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id,
+        timeOpen: timeOpen,
+        formattedWaitTime: formattedWaitTime
+      }
+    });
     case c.DELETE_TICKET:
       let newState = { ...state };
       delete newState[id];
